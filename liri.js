51d63964002e15d61b43d83c9206c3fb4a44d385
GiovanniLeonardo <40577375+GiovanniLeonardo@users.noreply.github.com>
2018-09-15 21:14:31 -0400

    update 2

--- liri.js
+++ liri.js
@@ -1,65 +1,49 @@
-//code to read and set environment variables with the dotenv package
-//require("dotenv").config();
+//read and set environment variables with the dotenv package
+require("dotenv").config();
 
-//var fs = require("fs"); //to call filesystem 
+var fs = require("fs"); //to call filesystem to read/write files
 
 //pull and store my personal keys into a variable (Keys).
 var keys = require("./keys.js"); 
 
-//Spotify API requests
-var Spotify = require("node-spotify-api"); 
+//API request to Spotify
+var Spotify = require("node-spotify-api");
 
-//Access my key information by runnin the Spotify request
-//var spotify = new Spotify(keys.spotify);
+//Access my key information 
+var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify({
-  id: '798de653df784a528f2b1d7e283bb21f',
-  secret: '680664038d534968aa4811c5237aff9d'
-});
-
-
-var userChoice = function(songName) {
-
+//take user input for song title
+var mySong = function(songTitle) {
 
 //show Artist, song name, preview link of the song, and the album that the song is from
-	spotify.search({ type: 'track', query: songName }, function(err, data){
-	 	if (err) {
-			console.log('Error occurred: ' + err);
-			return;
-	}
-	console.log(data);
-});
-}
+	spotify.search({ type: 'track', query: songTitle }, 
+	function(err, data) {
+		if (err) {
+			return console.log('Error occured: ' + err); 
+		} 
+		console.log(data.tracks.items[0]);		
+	
+// 		//Get songs and the attributes (artist, song name, url preview an album).
+ 		var songs = spotify 
+ 		for(var i = 0; i=songs.length; i++) {
+			console.log(i);
+			console.log('artist: ' + songs[i].artist);
+			console.log('song: ' + songs[i].name);
+			console.log('album: ' + songs[i].album.name);
+			console.log('preview: ' + songs[i].preview_url);
+ 		};
 
-// 		//variable to pull songs and the attributes (artist, song name, url preview an album).
-// 		var songs = data.tracks.items; 
-// 		for(var i = 0; i=songs.length; i++) {
-// 			console.log(i);
-// 			console.log('artist: ' + songs[i].artist.map(artist));
-// 			console.log('song: ' + songs[i].name);
-// 			console.log('preview: ' + songs[i].preview_url);
-// 			console.log('album: ' + songs[i].album.name);
-// 		}
-		
-// 	});
-// }	
 
-// var songPick = function(caseData, functionData) {
-// 	switch (caseData) {
-// 		case 'spotify-this-song':
-// 			userChoice(functionData);
-// 			// statements_1
-// 			break;
-// 		default:
-// 			// statements_def
-// 			break;
-// 				console.log("Liri does not recognize this ");
-//	}
-//	}
- // var run = function(argOne, argTwo) {
- // 	songPick(argOne, argTwo);
+var songPick = function(caseData, functionData) {
+ 	switch (caseData) {
+ 		case 'spotify-this-song':
+ 			mySong(functionData);
+ 			default:
+ 			console.log("Liri does not recognize this ");
+	 }
 
-// };
+   var getData = function(dataOne, dataTwo) {
+ 	songPick(dataOne, dataTwo);
 
  // } else // if no song is povied default to "The Sign" by Ace of Base.
  // default: 
@@ -70,7 +54,3 @@ var userChoice = function(songName) {
 
 // //create variable for holding the songs
 // var songlist = "";
-
-
-
-
