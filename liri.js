27631868ce477f798a95d744e7d228156d429ed8
GiovanniLeonardo <40577375+GiovanniLeonardo@users.noreply.github.com>
2018-09-16 22:16:46 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 
 var fs = require("fs"); //to call filesystem to read/write files
+var request = require("request");// request package
 
 //pull and store my personal keys into a variable (Keys).
 var keys = require("./keys.js"); 
@@ -12,45 +13,44 @@ var Spotify = require("node-spotify-api");
 //Access my key information 
 var spotify = new Spotify(keys.spotify);
 
+// store the arguments in the array
+var nodeArgs = process.argv;
+
+//create a variable to hold the artis name (arg: Name of venue, Location, Date of event (use moment to format this as MM/DD/YYYY))
+var artist = "";
+
+//create a variable to hold song name (arg: Artist, Song, Album, Preview Link)
+var songs = "";
+
+//create an empty variable for holding the movie name
+var movieName = "";
+
 //take user input for song title
 var mySong = function(songTitle) {
+	//if no song is provided, default to "The Sign" by Ace of Base.
+	if(songTitle == null )
+		value = "The Sign";
+	// console.log('title: ' + 'The Sign');
 
-//show Artist, song name, preview link of the song, and the album that the song is from
+//search song and get song data
 	spotify.search({ type: 'track', query: songTitle }, 
 	function(err, data) {
 		if (err) {
 			return console.log('Error occured: ' + err); 
 		} 
-		console.log(data.tracks.items[0]);		
-	
-// 		//Get songs and the attributes (artist, song name, url preview an album).
- 		var songs = spotify 
- 		for(var i = 0; i=songs.length; i++) {
+		console.log(data.tracks.items[0]);	
+	var getData = data.tracks.items;
+
+	//console log list of attributes (artist, song name, url preview an album).
+ 
+ 	  for(var i = 0; i<getData.length; i++) {
 			console.log(i);
-			console.log('artist: ' + songs[i].artist);
-			console.log('song: ' + songs[i].name);
-			console.log('album: ' + songs[i].album.name);
-			console.log('preview: ' + songs[i].preview_url);
+			console.log('artist: ' + getData[i].artist);
+			console.log('song: ' + getData[i].name);
+			console.log('album: ' + getData[i].album.name);
+			console.log('preview: ' + getData[i].preview_url);
  		};
+	})
+};
 
-
-var songPick = function(caseData, functionData) {
- 	switch (caseData) {
- 		case 'spotify-this-song':
- 			mySong(functionData);
- 			default:
- 			console.log("Liri does not recognize this ");
-	 }
-
-   var getData = function(dataOne, dataTwo) {
- 	songPick(dataOne, dataTwo);
-
- // } else // if no song is povied default to "The Sign" by Ace of Base.
- // default: 
- // console.log('title: ' spotify-this-song 'The Sign');
-
-// //store arguments in an array
-// var artist = process.argv;
-
-// //create variable for holding the songs
-// var songlist = "";
+mySong();
